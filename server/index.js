55e7550a1e573546c99e6211299a3cc913a9f5c1
Leonel Sicaro <leonelsicaro@gmail.com>
2024-05-04 09:02:23 +0200

    *** here we go again

---
+++ server/index.js
@@ -0,0 +1,88 @@
+require('dotenv').config()
+const express = require('express')
+const App = express()
+const mysql = require('mysql')
+const cors = require("cors")
+
+
+
+App.use(cors())
+App.use(express.json())
+const PORT= process.env.PORT || 5000
+const db = mysql.createConnection({
+    host: process.env.DB_HOST ||"localhost",
+    user: process.env.DB_USER ||"root",
+    port: process.env.DB_DATABASE ||"3306",
+    password: process.env.DB_PASSWORD ,
+    database: process.env.DB_DATABASE ||"usuarios"
+})
+
+App.post("/create", (req, res) => {
+    const nombre = req.body.nombre;
+    const edad = req.body.edad;
+    const pais = req.body.pais;
+    const cargo = req.body.cargo;
+    const antiguedad = req.body.antiguedad;
+
+    db.query('INSERT INTO usuarios(nombre,edad,pais,cargo,antiguedad) VALUES (?,?,?,?,?)', [nombre, edad, pais, cargo, antiguedad],
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send("usuario registrado")
+            }
+        }
+    )
+})
+App.get("/usuarios", (req, res) => {
+    db.query('SELECT * FROM usuarios',
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send(result)
+                }
+            }
+        )
+    }
+)
+App.put("/update", (req, res) => {
+    const id = req.body.id;
+    const nombre = req.body.nombre;
+    const edad = req.body.edad;
+    const pais = req.body.pais;
+    const cargo = req.body.cargo;
+    const antiguedad = req.body.antiguedad;
+    
+
+    db.query('UPDATE usuarios SET nombre=?, edad=? ,pais=?, cargo=?, antiguedad=? WHERE id=?', [nombre, edad, pais, cargo, antiguedad,id],
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send("usuario actualizado")
+            }
+        }
+    )
+})
+
+App.delete("/delete/:id", (req, res) => {
+    const id = req.params.id;
+    
+    
+
+    db.query('DELETE FROM usuarios WHERE id=?', [id],
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send(result)
+            }
+        }
+    )
+})
+
+
+App.listen(PORT, () => {
+    console.log(`listen on port ${PORT}`)
+})
